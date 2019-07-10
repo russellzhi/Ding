acf272c12c8511fc7f0d7ae4eacdfc3804e182e2
russellzhi <russellzhi@163.com>
2019-07-10 15:35:58 +0800

    add GitTest.php

---
+++ GitTest.php
@@ -0,0 +1,15 @@
+<?php
+/*********************************************************************************
+*	Hello buddy,you are awesome to choose sprint,this is a very light-weight php *
+*	framework with high performance and you will like it!						 *
+*********************************************************************************/
+namespace russellzhi\utility;
+class  GitTest {
+	public function say($str){
+		echo $str;
+	}
+	
+	public static function staticSay($str){
+		echo $str;
+	}
+}
